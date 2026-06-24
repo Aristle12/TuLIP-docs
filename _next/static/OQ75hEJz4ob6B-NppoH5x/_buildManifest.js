c75accf26b298938e293e7af8bedf4f42573e336
Aristle12 <46570490+Aristle12@users.noreply.github.com>
2026-06-24 16:05:39 +0000

    Deploying to gh-pages from @ Aristle12/mintlify-docs@2018c206f5c3cc9168dbd9518884bcc19bca707e 🚀

---
+++ _next/static/OQ75hEJz4ob6B-NppoH5x/_buildManifest.js
@@ -0,0 +1,22 @@
+self.__BUILD_MANIFEST = {
+  "/404": [
+    "./static/chunks/b889f9a818a186bc.js"
+  ],
+  "/500": [
+    "./static/chunks/0b7d265206b57eca.js"
+  ],
+  "/_error": [
+    "./static/chunks/852f3995d28f2ce4.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/404",
+    "/500",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
